45c6567d10e8edc49ca1094d836b9af0527784b3
SunnyVulpes <1791774501@qq.com>
2024-04-07 23:52:50 +0800

    init

---
+++ src/components/temp.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "temp"
+}
+</script>
+
+<template>
+  <div>这里写echarts</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/MainDashboard.vue
@@ -0,0 +1,20 @@
+<script>
+import Temp from "@/components/temp.vue";
+
+export default {
+  name: "MainDashboard",
+  components: {Temp}
+}
+</script>
+
+<template>
+  <div>这里是主页面的入口</div>
+  <div>整体dashboard的布局写在这里</div>
+
+<!--  temp为components文件夹下的组件，以下代码仅作示例-->
+  <el-card><temp></temp></el-card>
+</template>
+
+<style scoped>
+
+</style>
